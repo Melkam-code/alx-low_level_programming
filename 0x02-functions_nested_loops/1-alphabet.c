0eaaa9ae7cd14ce6d6764c7a7cf95ebcb68703c0
melkam <melkammekonnen899@gmail.com>
2022-03-15 06:45:17 -0700

    work

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
-#include main.h
+#include holberton.h
 /**
  * print_alphabet - prints the english alphabet from a-z.
  * Return: Nothing.
  */
 void print_alphabet(void)
 {
-README.md char c;
+char c;
 
-README.md for (c = 'a'; c <= 'z'; c++)
-README.md {
-README.md README.md _putchar(c);
-README.md }
-README.md _putchar('\n');
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
